7fb2ff96a1fad99d49aabf843718c627da76d6a4
Andrew Rogers <andrew.paul.rogers@gmail.com>
2019-02-04 19:32:44 -0600

    Added some code to convert the image to an np array.

--- mainScript.py
+++ mainScript.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import numpy
 
 # read the image in
 image = Image.open("earth_color.png")
@@ -6,5 +7,10 @@ image_grey = image.convert("L")
 image_blackwhite = image_grey.point(lambda x: 0 if x < 128 else 255, "1")
 
 processed_image = image_blackwhite.resize((200, 200))
-
 processed_image.show()
+
+
+image_matrix = numpy.array(processed_image)
+image_array = image_matrix.flatten()
+
+print(image_array)
